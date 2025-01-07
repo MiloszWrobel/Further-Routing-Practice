e834f50d765c812219b2baeaf2e6450589d4241b
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2025-01-07 17:07:39 +0000

    Loaded event details once clicked on event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import Root from "./pages/Root";
@@ -25,7 +27,11 @@ function App() {
               element: <EventsPage />,
               loader: eventsLoader,
             },
-            { path: ":eventId", element: <EventDetailPage /> },
+            {
+              path: ":eventId",
+              element: <EventDetailPage />,
+              loader: eventDetailLoader,
+            },
             { path: "new", element: <NewEventPage /> },
             { path: ":eventId/edit", element: <EditEventPage /> },
           ],
